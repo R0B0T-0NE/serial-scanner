ff447c26cf545d3d4880582a6bc72ea959704767
Suraj-Freshworks <suraj.gangwar@freshworks.com>
2023-04-07 19:52:30 +0530

    Create try.py

---
+++ try.py
@@ -0,0 +1,40 @@
+import requests
+import argparse
+
+
+parser = argparse.ArgumentParser("Argument Parser")
+parser.add_argument("token", metavar="token", type=str, help="auth_token")
+parser.add_argument("repo_name", metavar="repo_name", type=str, help="repo_name")
+
+args = parser.parse_args
+
+repo_name = args.repo_name
+token =args.token
+
+url = f'https://api.github.com/repos/{repo_name}/dependabot/alerts'
+
+payload={}
+headers = {
+  'Accept': 'application/vnd.github+json',
+  'X-GitHub-Api-Version': '2022-11-28',
+  'Authorization': 'Bearer '+token,
+}
+
+response = requests.request("GET", url, headers=headers, data=payload).json()
+
+count = 0 
+dependabotIssues = []
+for records in response:
+        description = (records['security_advisory']['description']).replace('\n','',1).replace('`','').replace('_','')
+        path = records['dependency']['manifest_path']
+        package = records['dependency']['package']['name']
+        severity = records['security_advisory']['severity']
+        vulnerableVersion = records['security_vulnerability']['vulnerable_version_range']
+        cvss = records['security_advisory']['cvss']['score']
+        summary = records['security_advisory']['summary']
+        advisory = records['security_advisory']['ghsa_id']
+        blank = ""
+        dependabotIssues.append([package, severity, cvss, summary, description, f'https://github.com/{repo_name}/tree/{branch_name}/'+path, 'https://github.com/advisories/'+advisory], blank, blank)
+        count += 1
+print(count)
+print(dependabotIssues)
